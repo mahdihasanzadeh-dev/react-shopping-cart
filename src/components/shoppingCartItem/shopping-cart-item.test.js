4d47eb5d839c202d57a1cc59b1dfe2bea3676b65
Mahdi Hasanzadeh <hasanzadehmahdi@gmail.com>
2021-12-22 13:24:14 +0330

    write test for shopping-cart-item component

--- src/components/shoppingCartItem/shopping-cart-item.test.js
+++ src/components/shoppingCartItem/shopping-cart-item.test.js
@@ -1,20 +1,35 @@
+import { DUMMY_DATA } from '../../data/DUMMY_DATA';
+import ShoppingCartContextProvider,{ShoppingCartContext} from '../../context/shopping-cart-context'
+import { fireEvent, render } from "@testing-library/react";
 
-import ShoppingCartContextProvider from '../../context/shopping-cart-context'
-import {
-    ShoppingCart
-} from '../index'
-import { mount, configure } from "enzyme";
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-configure({ adapter: new Adapter() });
-const wrapper = mount(
-    <ShoppingCartContextProvider >
-        <ShoppingCart />
-  </ShoppingCartContextProvider>
-)
-describe('<ShoppingCartItem />',()=>{
-    it('increment quantity item',()=>{
-     
-        wrapper.find("div.alt").first().simulate("click");
-      
+describe('<ShoppingCartItem/>',()=>{
+    it('Functionality',()=>{
+        const {getByText,getByTestId} =render(
+            <ShoppingCartContextProvider>
+                <ShoppingCartContext.Consumer>
+                    {
+                        (context)=>(
+                            <div >
+                                <div                
+                                    onClick={()=>context.decreaseQuantity(DUMMY_DATA[0].productId)}
+                                >decreaseQuantity</div>
+                                <div data-testid="quantity">{DUMMY_DATA[0].productQuantity}</div>
+                                <div                
+                                    onClick={()=>context.addQuantity(DUMMY_DATA[0].productId)}
+                                >addQuantity</div>
+                                 <span data-testid="price">{(DUMMY_DATA[0].productPrice * DUMMY_DATA[0].productQuantity).toFixed(2)}</span>
+                            </div>
+                        )
+                    }
+                </ShoppingCartContext.Consumer>
+            </ShoppingCartContextProvider>
+        )
+        fireEvent.click(getByText("decreaseQuantity"))
+        expect(getByTestId("quantity")).toHaveTextContent("0")
+        expect(getByTestId("price")).toHaveTextContent("0")
+        fireEvent.click(getByText("addQuantity"))
+        expect(getByTestId("quantity")).toHaveTextContent("1")
+        expect(getByTestId("price")).toHaveTextContent("10.50")
     })
+   
 })
